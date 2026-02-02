9e43a3bf098edceb88c81a3e0e46ecef46f3c51a
TomIUT <tomcox1108@gmail.com>
2026-02-02 15:55:11 +0100

    v1.0.1 - Nouveau readme et structure de fichiers

---
+++ Scoundrel/src/Symbole.java
@@ -0,0 +1,11 @@
+
+public enum Symbole {
+    PIQUE,TREFLE,COEUR,CARREAU;
+
+    boolean equals(String symbole){
+        if(this.equals(symbole)){
+            return true;
+        }
+        return false;
+    }
+}
